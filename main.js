71469c90e0ac280ee3552b2d65b20c5800909b25
Zahra Nabella El Kaffah <113093513+Zahranabella@users.noreply.github.com>
2025-01-08 19:30:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ var createLayer = function (title, layerName) {
   return new ol.layer.Tile({
     title: title,
     source: new ol.source.TileWMS({
-      url: " https://1fad-36-91-52-250.ngrok-free.app/geoserver/gisKotaLhokseumawe/wms",
+      url: "https://e27a-182-3-5-228.ngrok-free.app/geoserver/gisKotaLhokseumawe/wms",
       params: { LAYERS: "gisKotaLhokseumawe:" + layerName, TILED: true },
       serverType: "geoserver",
       visible: true,
